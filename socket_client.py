889b18d713f85c9b9881eaf998fbbe31e721d800
unknown <elizabeth.ykw@gmail.com>
2019-10-25 13:07:20 +0800

    linted & added missing try:

--- socket_client.py
+++ socket_client.py
@@ -1,16 +1,20 @@
 from websocket import create_connection
 from websocket._exceptions import WebSocketConnectionClosedException
 
-PORT='66666'
-def listen_for_message():    
-    ws=create_connection(f'ws://127.0.0.1:{PORT}')
+PORT = '66666'
+
+
+def listen_for_message():
+    ws = create_connection(f'ws://127.0.0.1:{PORT}')
     while True:
-        data=ws.recv()
-        if data:
-            #Do something
-            print(data)
+        try:
+            data = ws.recv()
+            if data:
+                # Do something
+                print(data)
         except WebSocketConnectionClosedException:
             listen_for_message()
-            
-if __name__=='__main__'    :
+
+
+if __name__ == '__main__':
     listen_for_message()
--- socket_host.py
+++ socket_host.py
@@ -1,26 +1,29 @@
 from tornado import websocket
 import tornado.ioloop
 
-PORT=66666
+PORT = 66666
+
+
 class EchoWebSocket(websocket.WebSocketHandler):
-    clients=set()
+    clients = set()
+
     def open(self):
         print('Websocket opened')
         self.clients.add(self)
-        
-    def on_message(self,message):
-        #Echo message to client
-#         self.write_message(f'Incoming message {message}')
-        #Broadcast message to all clients
+
+    def on_message(self, message):
+        # Echo message to client
+        #  self.write_message(f'Incoming message {message}')
+        # Broadcast message to all clients
         [c.write_message(f'{message}') for c in self.clients]
-        
+
     def on_close(self):
         print("websocket closed")
         self.clients.remove(self)
 
-application = tornado.web.Application([(r"/", EchoWebSocket),])
+
+application = tornado.web.Application([(r"/", EchoWebSocket), ])
 
 if __name__ == "__main__":
     application.listen(66666)
     tornado.ioloop.IOLoop.instance().start()
-        
